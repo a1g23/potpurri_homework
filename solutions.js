0be1acc7cfb6c8e1fc766b0a48e27a95caf84204
Alex Greenberg <acgreenberg2@gmail.com>
2023-11-16 21:29:27 -0500

    oddNumberCount completed

--- solutions.js
+++ solutions.js
@@ -17,3 +17,26 @@ setAlarm(true, true) // => returns false
 setAlarm(false, false) // => returns false
 setAlarm(false, true) // => returns false
 setAlarm(true, false) // => returns true
+
+
+
+// Write a function named oddNumberCount which receives one argument that should be a number. The function should return how many positive odd numbers there are below the number passed into the argument.
+
+// Example expected output:
+
+// for(i = 1; i < 7; i += 2) { // first can I write a loop that logs all odd numbers up to a point
+//         console.log(i)
+//     }
+
+
+const oddNumberCount = (number) => {
+    const values = []                          // I can create an array with no values in it
+    for(i = 1; i < number; i += 2) { 
+        values.push(i)                          // push the odd numbers into into it until the parameter via loop
+    }
+    console.log(values.length)                  // console.log the length of the array
+}
+
+
+oddNumberCount(7) // => returns 3 (because [1, 3, 5] are the positive odd numbers below 7)
+oddNumberCount(16) // => returns 8 (because [1, 3, 5, 7, 9, 11, 13, 15] are the positive odd numbers below 
